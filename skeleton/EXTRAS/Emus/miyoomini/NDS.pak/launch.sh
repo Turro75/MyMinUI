ff2f33eb8ab5953af195b103e74412d8c5119405
Turro75 <valerio.turrini@gmail.com>
2025-06-04 21:23:40 +0200

    Miyoomini: Add Steward-fu NDS support

--- skeleton/EXTRAS/Emus/miyoomini/NDS.pak/launch.sh
+++ skeleton/EXTRAS/Emus/miyoomini/NDS.pak/launch.sh
@@ -5,75 +5,31 @@ USE_752x560_RES=0
 
 mydir=$(dirname "$0")/drastic
 
+
+# set CPU speed
+#overclock.elf $CPU_SPEED_MENU
+overclock.elf $CPU_SPEED_GAME
+#overclock.elf $CPU_SPEED_PERF
+#overclock.elf $CPU_SPEED_MAX
+
+#inspired by from pico-8 wrapper onionos
 get_curvol() {
     awk '/LineOut/ {if (!printed) {gsub(",", "", $8); print $8; printed=1}}' /proc/mi_modules/mi_ao/mi_ao0
 }
-
-
-purge_devil() {
-    if pgrep -f "/dev/l" > /dev/null; then
-        echo "Process /dev/l is running. Killing it now..."
-        killall -9 l
-    else
-        echo "Process /dev/l is not running."
-    fi
-}
-
-cd $mydir
-if [ ! -f "/tmp/.show_hotkeys" ]; then
-    touch /tmp/.show_hotkeys
-    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
-fi
-
-export HOME=$mydir
-export PATH=$mydir:$PATH
-export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
-export SDL_VIDEODRIVER=mmiyoo
-export SDL_AUDIODRIVER=mmiyoo
-export EGL_VIDEODRIVER=mmiyoo
-
-
-
-
-#purge_devil
-
-if [  -d "/customer/app/skin_large" ]; then
-    USE_752x560_RES=1
-fi
-
-if [ "$USE_752x560_RES" == "1" ]; then
-    fbset -g 752 560 752 1120 32
-fi
-
-cd $mydir
-if [ "$CUST_LOGO" == "1" ]; then
-    ./png2raw
-fi
-
-sv=`cat /proc/sys/vm/swappiness`
-
-# 60 by default
-echo 10 > /proc/sys/vm/swappiness
-
-cd $mydir
-
-if [ "$CUST_CPUCLOCK" == "1" ]; then
-    overclock.elf $CPU_SPEED_PERF
-fi
 
 wait_for_device() {
     local start_time
     local elapsed_time
     start_time=$(date +%s)
     while [ ! -e /proc/mi_modules/mi_ao/mi_ao0 ]; do
-        sleep 0.2
+        sleep 0.1
         elapsed_time=$(( $(date +%s) - start_time ))
         if [ "$elapsed_time" -ge 4 ]; then
-        #echo "FAILED">>$SDCARD_PATH/audioserver.txt
+            #echo "FAILED">>$SDCARD_PATH/audioserver.txt
             return 1
         fi
     done
-    #keymon.elf &
+   # keymon.elf &
 }
 
 
@@ -85,6 +41,43 @@ set_snd_level() {
     echo "set_ao_volume 1 ${target_vol}" > /proc/mi_modules/mi_ao/mi_ao0
 }
 
+cd "${mydir}"
+if [ ! -f "/tmp/.show_hotkeys" ]; then
+    touch /tmp/.show_hotkeys
+    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
+fi
+
+
+TMP_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+export HOME=$mydir
+export PATH=$mydir:$PATH
+export LD_LIBRARY_PATH=$mydir/lib:$LD_LIBRARY_PATH
+export SDL_VIDEODRIVER=NDS
+export SDL_AUDIODRIVER=alsa
+
+
+
+
+#purge_devil
+
+if [  -d "/customer/app/skin_large" ]; then
+    USE_752x560_RES=0
+fi
+
+if [ "$USE_752x560_RES" == "1" ]; then
+    fbset -g 752 560 752 1120 32
+fi
+
+cd $mydir
+if [ "$CUST_LOGO" == "1" ]; then
+    ./png2raw
+fi
+
+if [ "$CUST_CPUCLOCK" == "1" ]; then
+    overclock.elf $CPU_SPEED_PERF
+fi
+
+
 curvol=$(get_curvol)
 #killall -9 keymon.elf
 if $IS_PLUS; then
@@ -95,10 +88,12 @@ fi
 sleep 0.2
 set_snd_level "${curvol}" &
 
-./drastic "$1"
+LD_TRACE_LOADED_OBJECTS=1 ./drastic	&> $LOGS_PATH/NDS_ldd.txt
+
+./drastic "$1"	&> $LOGS_PATH/NDS.txt
 sync
 
-echo $sv > /proc/sys/vm/swappiness
+
 
 if [  -d "/customer/app/skin_large" ]; then
     USE_752x560_RES=0
@@ -115,9 +110,9 @@ if $IS_PLUS; then
 	export LD_PRELOAD=/customer/lib/libpadsp.so
 else
 	if [ -f /customer/lib/libpadsp.so ]; then
-	    LD_PRELOAD=as_preload.so audioserver.mod &
-	    export LD_PRELOAD=libpadsp.so
+	     ${SDCARD_PATH}/.system/miyoomini/bin/audioserver.mod -60 &
 	fi
 fi
 wait_for_device &
 overclock.elf $CPU_SPEED_MENU
+LD_LIBRARY_PATH=$TMP_LD_LIBRARY_PATH
