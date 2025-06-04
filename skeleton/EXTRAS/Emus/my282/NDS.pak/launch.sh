81d07de8a66fbba9583cd137e962b9906e58db24
Turro75 <valerio.turrini@gmail.com>
2025-06-04 19:38:24 +0200

    my282: add working launch.sh for NDS

--- skeleton/EXTRAS/Emus/my282/NDS.pak/launch.sh
+++ skeleton/EXTRAS/Emus/my282/NDS.pak/launch.sh
@@ -1,111 +1,20 @@
 #!/bin/sh
-CUST_LOGO=0
-CUST_CPUCLOCK=1
-USE_752x560_RES=0
+MYDIR=`dirname "$0"`
 
-mydir=$(dirname "$0")/drastic
+export HOME="${MYDIR}/drastic"
+export SDL_VIDEODRIVER=NDS
 
-get_curvol() {
-    awk '/LineOut/ {if (!printed) {gsub(",", "", $8); print $8; printed=1}}' /proc/mi_modules/mi_ao/mi_ao0
-}
+overclock.elf ${CPU_SPEED_MAX}
+#sv=`cat /proc/sys/vm/swappiness`
+#echo 10 > /proc/sys/vm/swappiness
+#echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 
-
-purge_devil() {
-    if pgrep -f "/dev/l" > /dev/null; then
-        echo "Process /dev/l is running. Killing it now..."
-        killall -9 l
-    else
-        echo "Process /dev/l is not running."
-    fi
-}
-
-cd $mydir
-if [ ! -f "/tmp/.show_hotkeys" ]; then
-    touch /tmp/.show_hotkeys
-    LD_LIBRARY_PATH=./libs:/customer/lib:/config/lib ./show_hotkeys
-fi
-
-export HOME=$mydir
-export PATH=$mydir:$PATH
-export LD_LIBRARY_PATH=$mydir/libs:$LD_LIBRARY_PATH
-export SDL_VIDEODRIVER=mmiyoo
-export SDL_AUDIODRIVER=mmiyoo
-export EGL_VIDEODRIVER=mmiyoo
-
-
-
-
-#purge_devil
-
-if [  -d "/customer/app/skin_large" ]; then
-    USE_752x560_RES=1
-fi
-
-if [ "$USE_752x560_RES" == "1" ]; then
-    fbset -g 752 560 752 1120 32
-fi
-
-cd $mydir
-if [ "$CUST_LOGO" == "1" ]; then
-    ./png2raw
-fi
-
-sv=`cat /proc/sys/vm/swappiness`
-
-# 60 by default
-echo 10 > /proc/sys/vm/swappiness
-
-cd $mydir
-
-if [ "$CUST_CPUCLOCK" == "1" ]; then
-    overclock.elf $CPU_SPEED_PERF
-fi
-
-wait_for_device() {
-    local start_time
-    local elapsed_time
-    start_time=$(date +%s)
-    while [ ! -e /proc/mi_modules/mi_ao/mi_ao0 ]; do
-        sleep 0.2
-        elapsed_time=$(( $(date +%s) - start_time ))
-        if [ "$elapsed_time" -ge 4 ]; then
-        #echo "FAILED">>$SDCARD_PATH/audioserver.txt
-            return 1
-        fi
-    done
-    #keymon.elf &
-}
-
-
-set_snd_level() {
-    local target_vol="$1"
-    wait_for_device
-    echo "set_ao_mute 0" > /proc/mi_modules/mi_ao/mi_ao0
-    echo "set_ao_volume 0 ${target_vol}" > /proc/mi_modules/mi_ao/mi_ao0
-    echo "set_ao_volume 1 ${target_vol}" > /proc/mi_modules/mi_ao/mi_ao0
-}
-
-curvol=$(get_curvol)
-#killall -9 keymon.elf
-killall -9 audioserver
-sleep 0.2
-set_snd_level "${curvol}" &
-
-./drastic "$1"
+cd ${MYDIR}/drastic
+#LD_TRACE_LOADED_OBJECTS=1 LD_LIBRARY_PATH=${MYDIR}/drastic/lib:$LD_LIBRARY_PATH ./drastic &> $LOGS_PATH/NDS_ldd.txt
+#LD_LIBRARY_PATH=${MYDIR}/drastic/lib:$LD_LIBRARY_PATH ./drastic "$1" > ${LOGS_PATH}/NDS.txt 2>&1
+./lib/ld-linux-armhf.so.3 --library-path lib ./drastic "$1" > ${LOGS_PATH}/NDS.txt 2>&1
 sync
 
-echo $sv > /proc/sys/vm/swappiness
-
-if [  -d "/customer/app/skin_large" ]; then
-    USE_752x560_RES=0
-fi
-
-if [ "$USE_752x560_RES" == "1" ]; then
-    fbset -g 640 480 640 960 32
-fi
-
-#killall -9 keymon.elf
-sleep 0.2
-/customer/app/audioserver -60 &
-wait_for_device &
-overclock.elf $CPU_SPEED_MENU
+#echo $sv > /proc/sys/vm/swappiness
+#echo ondemand > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+overclock.elf ${CPU_SPEED_MENU}
